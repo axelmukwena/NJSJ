cc1c23913e1c662d32e594ff582d6a3768cdda99
Carlos Sibalatani <carlos.sibalatani@mhss.gov.na>
2021-08-23 18:25:18 +0200

    More changes

--- src/js/getLatest.js
+++ src/js/getLatest.js
@@ -6,7 +6,7 @@ function fetchData() {
 
             return volume
         }).then((data) => {
-            document.querySelector('#latestVolLink').insertAdjacentHTML('beforeend', `<a href="/volumes/editorial/${data._id}" class="btn btn-primary" id="latestLink">Download editorial</a> <a href="/volume/${data._id}" class="btn btn-primary" id="latestLink">View Articles</a>`)
+            document.querySelector('#latestVolLink').insertAdjacentHTML('beforeend', `<a href="/volumes/editorial/${data._id}" class="btn btn-primary" id="latestLink">Download Volume 1</a> <a href="/volume/${data._id}" class="btn btn-primary" id="latestLink">View Articles</a>`)
             document.querySelector('#volumeCover').innerHTML = `<a href="/volumes/cover/${data._id}"><img src="/volumes/cover/${data._id}" alt=""></a>`
             document.querySelector('#LatestVolume').innerHTML = `<h1>${data.name}</h1>`
             document.querySelector('#LatestVolumeTitle').innerHTML = `<h2>${data.title}</h2>`
