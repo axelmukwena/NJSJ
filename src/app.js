3f51793da871bfac5a570fa77f09c2e2de6c84a8
Axel Mukwena <axel.muk@gmail.com>
2022-11-11 14:14:15 +0200

    Forl, add prettier and formats, database, etc

--- src/app.js
+++ src/app.js
@@ -1,103 +1,106 @@
-const express = require('express')
-const path = require('path')
-const hbs = require('hbs')
-const cookieParser = require('cookie-parser')
-const bodyParser =require('body-parser')
-const nodemailer = require('nodemailer')
-const userRoute = require('./routers/user')
-const volumeRoute = require('./routers/volume')
-const articlesRoute = require('./routers/article')
-const mongoose = require('./database/mongoose')
-const {volumes} = require('./models/volume')
-const {articles} = require('./models/article')
-const auth = require('../src/middleware/auth')
+// Loads the configuration
+require("dotenv").config();
 
-const publicPath = path.join(__dirname,'../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname,'../templates/partials')
-const scriptPath = path.join(__dirname,'../src/js')
-const app = express()
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const userRoute = require("./routers/user");
+const volumeRoute = require("./routers/volume");
+const articlesRoute = require("./routers/article");
+const mongoose = require("./database/mongoose");
+const { volumes } = require("./models/volume");
+const { articles } = require("./models/article");
+const auth = require("../src/middleware/auth");
 
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-app.use('/js',express.static(scriptPath))
-app.use(cookieParser())
-hbs.registerPartials(partialsPath)
+const publicPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+const scriptPath = path.join(__dirname, "../src/js");
+const app = express();
 
-app.use(express.static(publicPath))
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+app.use("/js", express.static(scriptPath));
+app.use(cookieParser());
+hbs.registerPartials(partialsPath);
 
-app.use(express.json())
-app.use(userRoute)
-app.use(volumeRoute)
-app.use(articlesRoute)
+app.use(express.static(publicPath));
 
-const urlencodedParser = bodyParser.urlencoded({extended: false})
+app.use(express.json());
+app.use(userRoute);
+app.use(volumeRoute);
+app.use(articlesRoute);
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'namsocialjustice@gmail.com',
-        pass: 'thhqujpnjhnyibws'
-    }
-})
+  service: "gmail",
+  auth: {
+    user: "namsocialjustice@gmail.com",
+    pass: "thhqujpnjhnyibws",
+  },
+});
 
-app.get('',(req, res) => {
-    res.render('index')
-})
+app.get("", (req, res) => {
+  res.render("index");
+});
 
-app.get('/about', (req, res) => {
-    res.render('about')
-})
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
-app.get('/login', (req, res) => {
-    res.render('login')
-})
-app.get('/signup', (req, res) => {
-    res.render('signup')
-})
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
 
-app.get('/volume/:id', (req, res) => {
-    res.render('volume',{
-        "volume": req.params.id
-    })
-})
-app.get('/guidelines',(req, res)=> {
-    res.render('guidelines')
-})
-app.get('/submission',(req, res)=> {
-    res.render('submission')
-})
-app.get('/search', async (req, res)=>{
-    const query = {$text: {$search: req.query.term}}
-    const articleAll = await articles.find(query)
+app.get("/volume/:id", (req, res) => {
+  res.render("volume", {
+    volume: req.params.id,
+  });
+});
+app.get("/guidelines", (req, res) => {
+  res.render("guidelines");
+});
+app.get("/submission", (req, res) => {
+  res.render("submission");
+});
+app.get("/search", async (req, res) => {
+  const query = { $text: { $search: req.query.term } };
+  const articleAll = await articles.find(query);
 
-    if(!articleAll) {
-        res.render('search',{result: "none"})
-    }
-    res.render('search', {result: req.query.term})
-})
+  if (!articleAll) {
+    res.render("search", { result: "none" });
+  }
+  res.render("search", { result: req.query.term });
+});
 
-app.get('/search/:term', async (req, res)=>{
-    const query = {$text: {$search: req.params.term}}
-    const articleAll = await articles.find(query)
+app.get("/search/:term", async (req, res) => {
+  const query = { $text: { $search: req.params.term } };
+  const articleAll = await articles.find(query);
 
-    res.send(articleAll)
-})
-app.get('/contact',(req, res)=> {
-    res.render('contact')
-})
-app.post('/contactus', urlencodedParser, (req, res)=>{
-    try {
-        const today = Date.now()
-        const date = new Date(today)
-        const maillist = ['njsjnam@gmail.com']
+  res.send(articleAll);
+});
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+app.post("/contactus", urlencodedParser, (req, res) => {
+  try {
+    const today = Date.now();
+    const date = new Date(today);
+    const maillist = ["njsjnam@gmail.com"];
 
-        maillist.forEach((to)=>{
-            const mailOptions = {
-                from: 'namsocialjustice@gmail.com',
-                to: to,
-                subject: 'Query from namsocialjustice.org',
-                text: `
+    maillist.forEach((to) => {
+      const mailOptions = {
+        from: "namsocialjustice@gmail.com",
+        to: to,
+        subject: "Query from namsocialjustice.org",
+        text: `
                     From: ${req.body.email}
     
                     Name: ${req.body.firstname} ${req.body.surname}
@@ -105,31 +108,28 @@ app.post('/contactus', urlencodedParser, (req, res)=>{
                     Message: ${req.body.message}
     
                     Sent on: ${date}
-                `
-            }
-            
-            transporter.sendMail(mailOptions, (error, info) => {
-                if(error) res.render('404')
-                else res.render('contact')
-            })
-        })
+                `,
+      };
 
-    } catch (error) {
-        
-    }
-})
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) res.render("404");
+        else res.render("contact");
+      });
+    });
+  } catch (error) {}
+});
 
-app.get('/publish/volume', auth, (req, res) =>{
-    res.render('publishVolume')
-})
+app.get("/publish/volume", auth, (req, res) => {
+  res.render("publishVolume");
+});
 
-app.get('/publish/article/:id', auth, (req, res) => {
-    res.render('publishArticles', {
-        volume: req.params.id
-    })
-})
+app.get("/publish/article/:id", auth, (req, res) => {
+  res.render("publishArticles", {
+    volume: req.params.id,
+  });
+});
 
-app.get('*', (req, res)=>{
-    res.render('404')
-})
-module.exports = app
+app.get("*", (req, res) => {
+  res.render("404");
+});
+module.exports = app;
--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,8 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/NJSJ'
+const connectionString = process.env.MONGOLAB_URI;
+const database = process.env.MONGODB;
 
-mongoose.connect(URL,{
-    useNewUrlParser: true,
-    useCreateIndex: true
-})
+// MongoDB Connection Pools
+const options = {
+  autoIndex: false, // Don't build indexes
+  maxPoolSize: 490, // Maintain up to 10 socket connections. Mongo free is 500 Max
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4, // Use IPv4, skip trying IPv6
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  dbName: database,
+};
+
+mongoose
+  .connect(connectionString, options)
+  .then(() => console.log("Successfully connected to MongoDB."))
+  .catch((err) => console.log("Error:", err));
